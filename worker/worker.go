adccb1dfe77abc5a0ed3fb3ae9ff5bf3417e5b0a
avoforme <hophuongthanh028@gmail.com>
2026-04-08 16:01:42 -0400

    Complete worker's code

--- worker/worker.go
+++ worker/worker.go
@@ -4,8 +4,52 @@
 // request a job, run it locally, report the result.
 package worker
 
+import (
+	"cs4513/project1/jobs"
+	"cs4513/project1/types"
+	"fmt"
+	"net/rpc"
+	"time"
+)
+
 // Run connects to the coordinator and processes jobs until it hits an error.
 func Run(addr string) error {
 	// TODO: implement
+	client, err := rpc.Dial("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("dial %s: %w", addr, err)
+	}
+	defer client.Close()
+	
+	var reply types.WorkerID
+	err = client.Call("Coordinator.Register", types.WorkerInfo{}, &reply)
+	if err != nil {
+		return fmt.Errorf("Fail to register worker")
+	}
+	workerID := reply
+
+	for {
+		var newJob types.Job
+		err = client.Call("Coordinator.RequestJob", workerID, &newJob)
+		if (err != nil) {
+			if types.IsNoWork(err) {
+    			time.Sleep(1 * time.Second)
+				continue
+			} else { return fmt.Errorf("Worker not registerd") }
+		}
+
+		result := jobs.ExecuteJob(newJob)
+		result.WorkerID = workerID
+		if result.Err != "" {
+			print(result.Err)
+		}
+		
+		err = client.Call("Coordinator.ReportResult", result, &struct{}{})
+		if err != nil {
+			return fmt.Errorf("Job not found")
+		}
+
+	}
+
 	return nil
 }
