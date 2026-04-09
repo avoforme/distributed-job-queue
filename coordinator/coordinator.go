0adc0a4936b89bbe718a84af9c7d5df3a553c371
avoforme <hophuongthanh028@gmail.com>
2026-04-09 17:09:45 -0400

    Implement priority queue

--- coordinator/coordinator.go
+++ coordinator/coordinator.go
@@ -10,11 +10,49 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+	"container/heap"
 )
+
+type Item struct {
+	jobID    types.JobID
+	priority int   
+	seq int
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less (i, j int) bool {
+	if pq[i].priority != pq[j].priority {
+		return pq[i].priority > pq[j].priority
+	}
+	return pq[i].seq < pq[j].seq
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	item := x.(*Item)
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	*pq = old[0 : n-1]
+	return item
+}
 
 // jobRecord is the coordinator's private copy of a job and its status.
 type jobRecord struct {
 	// TODO: add fields
+	seq int
 	job types.Job
 	jobStatus types.JobStatus
 }
@@ -25,7 +63,8 @@ type Coordinator struct {
 	// TODO: add fields
 	mu sync.Mutex
 	jobs map[types.JobID] *jobRecord
-	queue []types.JobID
+	// queue []types.JobID
+	queue PriorityQueue
 	workers map[types.WorkerID] time.Time
 	nextJobID int64
 	nextWorkerID int64
@@ -34,13 +73,16 @@ type Coordinator struct {
 // New returns an initialized Coordinator.
 func New() *Coordinator {
 	// TODO: implement
-	return &Coordinator{
+	c := Coordinator{
 		jobs: make(map[types.JobID]*jobRecord),
 		workers: make(map[types.WorkerID]time.Time),
-		queue: []types.JobID{},
+		// queue: []types.JobID{},
+		queue: PriorityQueue{},
 		nextJobID: 0,
 		nextWorkerID: 0,
 	}
+	heap.Init(&c.queue)
+	return &c
 }
 
 // Start creates a Coordinator, registers it, and starts listening on addr.
@@ -70,7 +112,8 @@ func (c *Coordinator) SubmitJob(spec types.JobSpec, reply *types.JobID) error {
 	// TODO: implement
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	newJobID := types.JobID(fmt.Sprintf("job-%d", atomic.AddInt64(&c.nextJobID, 1)))
+	newSequence := atomic.AddInt64(&c.nextJobID, 1)
+	newJobID := types.JobID(fmt.Sprintf("job-%d", newSequence))
 	newJob := types.Job{
 		ID: newJobID,
 		Spec: spec,
@@ -80,13 +123,19 @@ func (c *Coordinator) SubmitJob(spec types.JobSpec, reply *types.JobID) error {
 		State: types.StatePending,
 	}
 	newJobEntry := jobRecord{
+		seq: int(newSequence),
 		job: newJob,
 		jobStatus: newJobStatus,
 	}
 
 	// Go automatically dereferences pointers when accessing fields.
 	c.jobs[newJobID] = &newJobEntry
-	c.queue = append(c.queue, newJobID)
+	// c.queue = append(c.queue, newJobID)
+	heap.Push(&c.queue, &Item{
+		jobID: newJobID,
+		priority: spec.Priority,
+		seq: int(newSequence),
+	})
 
 	*reply = newJobID
 	return nil
@@ -149,8 +198,10 @@ func (c *Coordinator) RequestJob(workerID types.WorkerID, reply *types.Job) erro
    		return types.ErrNoWork
 	}
 	
-	job := c.jobs[c.queue[0]]
-	c.queue = c.queue[1:]
+	// job := c.jobs[c.queue[0]]
+	// c.queue = c.queue[1:]
+	item := heap.Pop(&c.queue).(*Item)
+	job := c.jobs[item.jobID]
 
 	job.jobStatus.State = types.StateRunning
 	job.jobStatus.WorkerID = workerID
